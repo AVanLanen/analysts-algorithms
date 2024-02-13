d73229a8f0603c0f8bcc8071c6c1e5c2cbb834ae
Andrew Van Lanen <andrew.vanlanen@kingsmensoftware.com>
2024-02-13 09:35:03 -0500

    Changed background

--- src/app/chat/page.tsx
+++ src/app/chat/page.tsx
@@ -10,10 +10,11 @@ export default function Chat() {
   return (
     
     <>
-    <main className="mx-auto w-full h-screen max-w-lg p-24 flex flex-col">
+    <body className="bg-gradient-to-r from-green-400 to-blue-500 w-full">
+    <main className="mx-auto w-full h-screen max-w-lg p-24 flex flex-col bg-primary">
         <h1 className="text-4xl font-bold mb-4">Kingsmen Chat</h1>
         <p className="text-xl mb-4">Let me know how I can be of assistance</p>
-      <div className="relative flex place-items-center before:absolute before:h-[300px] before:w-[480px] before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-[240px] after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700 before:dark:opacity-10 after:dark:from-sky-900 after:dark:via-[#0141ff] after:dark:opacity-40 before:lg:h-[360px] z-[-1]">
+      <div className="relative flex place-items-center">
         </div>
       <section className="mb-auto m">
         {messages.map(m => (
@@ -38,6 +39,7 @@ export default function Chat() {
         </button>
       </form>
     </main>
+    </body>
     </>
   );
 }
